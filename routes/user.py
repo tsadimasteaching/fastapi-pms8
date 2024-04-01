592cdf59f72dfb306d0c93f93e6a5d56b0d0a014
Anargyros Tsadimas <tsadimas@hua.gr>
2024-04-01 18:18:14 +0300

    add get user and delete user

--- routes/user.py
+++ routes/user.py
@@ -19,5 +19,29 @@ async def create_user(user: UserBase, session: AsyncSession = Depends(get_sessio
     await session.commit()
     await session.refresh(dbuser)
     return dbuser
+
+@router.get("/{user_id}")
+async def get_user(user_id: int, session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(User).filter(User.id == user_id))
+    user = result.scalars().first()
+    return user
+
+@router.put("/{user_id}")
+async def update_user(user_id: int, user: UserBase, session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(User).filter(User.id == user_id))
+    dbuser = result.scalars().first()
+    print(f"User: {dbuser}")
+    dbuser.name = user.name
+    dbuser.surname = user.surname
+    dbuser.email = user.email
+    await session.commit()
+    return dbuser
     
-    
+@router.delete("/{user_id}")
+async def delete_user(user_id: int, session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(User).filter(User.id == user_id))
+    dbuser = result.scalars().first()
+    print(f"User: {dbuser}")
+    await session.delete(dbuser)
+    await session.commit()
+    return dbuser
