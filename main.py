a74f2d53cae5c33ccedb5967b97f5ebcd943812f
Anargyros Tsadimas <tsadimas@hua.gr>
2024-04-02 16:53:14 +0300

    added keycloak -wip

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from models import User
 from typing import List
 from db import init_db
 from routes.user import router as userrouter
 from routes.job import router as jobrouter
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from jwt import PyJWKClient
+import jwt
+from typing import Annotated
+
 
 app = FastAPI()
 
-user1 = User(name="Nikos", surname="Tsertos", email="nick@hua.gr")
+oauth_2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl="http://keycloak:8080/realms/hulk/protocol/openid-connect",
+    authorizationUrl="http://keycloak:8080/realms/hulk/protocol/openid-connect/auth",
+    refreshUrl="http://keycloak:8080/realms/hulk/protocol/openid-connect/token",
+)
 
-userlist = []
-userlist.append(user1)
+
+async def valid_access_token(
+    access_token: Annotated[str, Depends(oauth_2_scheme)]
+):
+    url = "http://keycloak:8080/realms/hulk/protocol/openid-connect/certs"
+    optional_custom_headers = {"User-agent": "custom-user-agent"}
+    jwks_client = PyJWKClient(url, headers=optional_custom_headers)
+
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience="api",
+            options={"verify_exp": True},
+        )
+        return data
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Not authenticated")
 
 @app.on_event("startup")
 async def startup():
     await init_db()
 
-
+@app.get("/private", dependencies=[Depends(valid_access_token)])
+def get_private():
+    return {"message": "Ce endpoint est privé"}
 
 app.include_router(userrouter, prefix='/user', tags=["User"])
 app.include_router(jobrouter, prefix='/job', tags=["Job"])
